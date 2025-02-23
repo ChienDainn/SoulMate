c12d99185f70e734f9c08cf8e4ea8c7048d7376d
ChienDainn <chien.minh001.com>
2025-02-24 01:03:20 +0700

    Updating the users controller

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+66a827506edfc9296642c8f9c1f38d7ba04c5b32")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a2b4534304404eac2af2007174579d057c71d29")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
